ae42cde09e6632e93060ebf86aff99909c0aee16
harijoshi12 <mr.hariprasadjoshi@gmail.com>
2022-10-23 18:00:01 +0530

    maintenance mode added improved

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,28 @@ import '../styles/globals.css'
 import '../components/Header/Header.css'
 import '../components/Footer/Footer.css'
 import AuthContextProvider from '../contexts/AuthContext'
+import { useState } from 'react'
 function MyApp({ Component, pageProps }) {
+  const [isMaintenance, setIsMaintenance] = useState(true)
+  const h1Style = {
+    width: "100%",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column"
+  }
   return (
     <AuthContextProvider>
       <Provider store={store}>
-        <Component {...pageProps} />
+        {!isMaintenance ? (
+          <Component {...pageProps} />
+        ) : (
+          <div style={h1Style}>
+            <h1> Site is being updated ...</h1>
+            <button style={{ padding: "15px", margin: "15px", cursor: "pointer" }} onClick={() => setIsMaintenance(false)}> Anyway view beta version</button>
+          </div>
+        )}
       </Provider>
     </AuthContextProvider>
   )
--- pages/index.jsx
+++ pages/index.jsx
@@ -10,15 +10,7 @@ import Testimonial from "../components/home/Testimonial/Testimonial";
 import styles from "../styles/Home.module.css";
 import Layout from "../UI/Layout";
 export default function Home() {
-  const [isMaintenance, setIsMaintenance] = useState(true)
-  const h1Style = {
-    width: "100%",
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column"
-  }
+
   return (
     <Fragment>
       <Head>
@@ -29,22 +21,15 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {!isMaintenance ? (
-
-        <Layout>
-          <main className={styles.home_main}>
-            <Hero />
-            <Features />
-            <Procedure />
-            {/* <Testimonial/> */}
-            <Contact />
-          </main>
-        </Layout>) : (
-        <div style={h1Style}>
-          <h1> Site is being updated ...</h1>
-          <button style={{ padding: "15px", margin: "15px", cursor: "pointer" }} onClick={() => setIsMaintenance(false)}> Anyway view beta version</button>
-        </div>
-      )}
+      <Layout>
+        <main className={styles.home_main}>
+          <Hero />
+          <Features />
+          <Procedure />
+          {/* <Testimonial/> */}
+          <Contact />
+        </main>
+      </Layout>
     </Fragment>
   );
 }
