0173ab97d8e04b94f108b157c599288fbb8c284f
harijoshi12 <mr.hariprasadjoshi@gmail.com>
2022-10-23 17:54:22 +0530

    maintenance mode added

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useState } from "react";
 import { Fragment } from "react";
 import Contact from "../components/home/Contact/Contact";
 import Features from "../components/home/Features/Features";
@@ -9,6 +10,15 @@ import Testimonial from "../components/home/Testimonial/Testimonial";
 import styles from "../styles/Home.module.css";
 import Layout from "../UI/Layout";
 export default function Home() {
+  const [isMaintenance, setIsMaintenance] = useState(true)
+  const h1Style = {
+    width: "100%",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column"
+  }
   return (
     <Fragment>
       <Head>
@@ -19,15 +29,22 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Layout>
-        <main className={styles.home_main}>
-          <Hero />
-          <Features />
-          <Procedure />
-          {/* <Testimonial/> */}
-          <Contact />
-        </main>
-      </Layout>
+      {!isMaintenance ? (
+
+        <Layout>
+          <main className={styles.home_main}>
+            <Hero />
+            <Features />
+            <Procedure />
+            {/* <Testimonial/> */}
+            <Contact />
+          </main>
+        </Layout>) : (
+        <div style={h1Style}>
+          <h1> Site is being updated ...</h1>
+          <button style={{ padding: "15px", margin: "15px", cursor: "pointer" }} onClick={() => setIsMaintenance(false)}> Anyway view beta version</button>
+        </div>
+      )}
     </Fragment>
   );
 }
