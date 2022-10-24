d010f828149b45f33cb810c68f2f1a287189b588
harijoshi12 <mr.hariprasadjoshi@gmail.com>
2022-10-24 18:47:58 +0530

    loader added to dashboard page

--- components/templates/template1/edit/Template1.jsx
+++ components/templates/template1/edit/Template1.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { ThreeDots } from 'react-loader-spinner'
 import { useAuth } from '../../../../contexts/AuthContext'
 import { getResumeAsync, STATUSES, updateResume, updateResumeAsync } from '../../../../features/resume/resumeSlice'
 import { NewSecContactDetails, NewSecEducations, NewSecExperiences, NewSecInterests, NewSecLanguages, NewSecPersonalInfo, NewSecProgLangs, NewSecProjects, NewSecTechnicalSkills, } from './Sections'
 import styles from '../styles/Template1.module.css'
 
 const Template1 = () => {
-
   const { currentToken, currentUser } = useAuth();
 
   const dispatch = useDispatch()
@@ -22,7 +22,31 @@ const Template1 = () => {
 
   const { status, data } = useSelector(state => state.resume)
   if (status === STATUSES.PENDING) {
-    return <h2>Loading</h2>
+    return (
+      <div style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "rgba(255, 255,255,.5)",
+        zIndex: "1000"
+      }}>
+        <ThreeDots
+          height="80"
+          width="80"
+          radius="9"
+          color="#4fa94d"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{}}
+          wrapperClassName=""
+          visible={true}
+        />
+      </div>
+    )
   }
 
   const tempStyle = {
