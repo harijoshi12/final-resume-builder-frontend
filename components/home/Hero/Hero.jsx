96a357893d36598496721988b13247b257ce35fb
harijoshi12 <mr.hariprasadjoshi@gmail.com>
2022-10-24 18:59:18 +0530

    create your resum btn redirect added

--- components/home/Hero/Hero.jsx
+++ components/home/Hero/Hero.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import styles from "../../../styles/Home.module.css";
 
@@ -9,7 +10,7 @@ const Hero = () => {
           <div className={styles.left_section}>
             <h1>The Best free online Resume Builder</h1>
             <p>Build your brand-new resume in as little as 5 minutes</p>
-            <button>Create Your Resume</button>
+            <Link href="/resume-templates"><button>Create Your Resume</button></Link>
           </div>
           <div className={styles.right_section}>
             <div className={styles.resumebg}>
